2bc3b4f0b73e8e2d4fb36015a12475758ee5c556
blockyboy696 <41006167+blockyboy696@users.noreply.github.com>
2022-12-23 15:23:16 +0300

    Delete pg_connector.py

--- connector/pg_connector.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from conf.conf import logging
-
-def get_data(link: str) -> pd.DataFrame:
-    logging.info('extractinng data')
-    df = pd.read_csv(link)
-    logging.info('data extracted & df created')
-    return df 
