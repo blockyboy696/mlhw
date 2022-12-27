e048e74b4a54df742280270c15f8282efa7bab8b
Kirill Gorbunov <blockyboy696@gmail.com>
2022-12-28 00:06:52 +0300

    fixed an error

--- entrypoint.py
+++ entrypoint.py
@@ -30,6 +30,5 @@ lr_params = gridsearch_lr(X_train, y_train)
 
 lr = training_model(LogisticRegression, X_train, y_train, **lr_params)
 
-prediction_data = [[59, 1, 0, 101, 234, 0, 1, 143, 0, 3.4, 0, 0, 0]]
 
 predict(args.ModelPath, (np.array(args.Data)).reshape(1, -1))
